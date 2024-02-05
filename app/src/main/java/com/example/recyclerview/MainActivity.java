fd646ed87db8cd85c40a6397c80ecf9429ccb5b8
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-02-06 03:07:11 +0600

    Second Commit RecyclerView

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    int[] images = {R.drawable.afghanistan_icon,R.drawable.armenia_icon,R.drawable.azerbaijan_icon,R.drawable.bahrain_icon,
+            R.drawable.bangladesh_icon,R.drawable.bhutan_icon,R.drawable.china_icon,R.drawable.indonesia_icon,R.drawable.india_icon,
+            R.drawable.iran_icon,R.drawable.iraq_icon,R.drawable.japan_icon,R.drawable.kazakhstan_icon,R.drawable.nepal_icon,
+            R.drawable.pakistan_icon,R.drawable.sri_lanka_icon,R.drawable.turkey_icon
+
+    };
+
+    MyAdapter myAdapter;
+
+    String[] title,desc;
+
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView_Id);
+
+        title = getResources().getStringArray(R.array.county_names);
+        desc = getResources().getStringArray(R.array.county_desc);
+
+
+        myAdapter = new MyAdapter(this,title,desc,images);
+
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
+
+
+
+
+
+
+
+
